2bc40ee54caa3b45eb7caec8a22d5fdb05a4abe5
pengmengmeng <pengmengmeng@meituan.com>
2019-08-01 08:00:46 +0800

    first commit

---
+++ client/entries/Detail.js
@@ -0,0 +1,4 @@
+import render from '../utils/render';
+import Detail from '../pages/Detail';
+
+render(Detail);
---
+++ client/entries/Home.js
@@ -0,0 +1,4 @@
+import render from '../utils/render';
+import Home from '../pages/Home';
+
+render(Home);
